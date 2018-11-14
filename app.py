d9d9b809c02ebcd25abb07590df5efd64c346ac9
TylerTomasello <ttomasel@hawk.iit.edu>
2018-11-14 16:48:32 -0600

    start the python application to run the site

---
+++ app.py
@@ -0,0 +1,31 @@
+import flask
+import logging
+import pymysql.cursors
+
+from flask import (Flask,
+                   render_template,
+                   request,
+                   session)
+
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+    return render_template("index.html")
+
+
+def main():
+    #logger.info("Loading app with Flask version {}".format(flask.__version__))
+
+    #register_apis(app, db_connection_manager.db)
+
+    #app.logger.handlers = logger.handlers
+    #app.logger.setLevel(logging.DEBUG)
+
+    app.run(host="0.0.0.0",
+            port=9000,
+            use_reloader=True,
+            debug=True
+            )
