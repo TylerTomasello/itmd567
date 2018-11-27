320029c805287885afeffa0ef0096e606d273a79
TylerTomasello <ttomasel@hawk.iit.edu>
2018-11-27 15:07:33 -0600

    add the current working with vue

--- static/main.js
+++ static/main.js
@@ -1,7 +1,35 @@
+import Vue from 'vue'
+
 function main()
 {
-  Vue.options.delimiters = ["[[", "]]"];
-  
+  //Vue.options.delimiters = ["[[", "]]"];
+  const routes = [
+        {
+            path: "/",
+            component: homeView,
+            props: true
+        },
+        {
+            path: "/about/",
+            name: "about",
+            component:
+        }
+    ];
+
+    const router = new VueRouter({
+        routes // short for `routes: routes`
+    });
+
+    var app = new Vue({
+        el: "#mainApp",
+        name: "mainApp",
+        router,
+
+        data: {
+          message: 'hello!!!'
+            //loggedIn: true,
+        }
+    });
 }
 
 $(document).ready(main);
