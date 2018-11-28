dfa436fca8af4852484da1827a0265000451754d
TylerTomasello <ttomasel@hawk.iit.edu>
2018-11-28 15:18:59 -0600

    finished form with date picker as well

--- static/main.js
+++ static/main.js
@@ -91,6 +91,15 @@ function main()
             //loggedIn: true,
         }
     });
+
+    var date_input=$('input[name="date"]'); //our date input has the name "date"
+    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+    date_input.datepicker({
+        format: 'mm/dd/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+      })
  }
 
 $(document).ready(main);
